0e8c9195cef37bfbc4926de6f39dbc149415b4e1
vssworkteam <vssworkteam@gmail.com>
2026-05-03 00:16:23 +0800

    0503

--- js/bracket.js
+++ js/bracket.js
@@ -21,24 +21,24 @@ const data = {
       a:{ name:"BKS", score:2, win:true },
       b:{ name:"FF", score:0, win:false }
     },
-    { id:"ubf", x:430, y:188, bo:3, date:"05/02 21:30", state:"未開始",
-      a:{ name:"NOOB", score:null, win:false },
-      b:{ name:"BKS", score:null, win:false }
+    { id:"ubf", x:430, y:188, bo:3, date:"05/02 21:30", state:"已結束",
+      a:{ name:"NOOB", score:2, win:true },
+      b:{ name:"BKS", score:0, win:false }
     },
 
     // 敗部
-    { id:"lb1", x:0, y:588, bo:3, date:"05/02 19:00", state:"未開始",
-      a:{ name:"EDGE", score:null, win:false },
-      b:{ name:"FF", score:null, win:false }
+    { id:"lb1", x:0, y:588, bo:3, date:"05/02 19:00", state:"已結束",
+      a:{ name:"EDGE", score:2, win:true },
+      b:{ name:"FF", score:1, win:false }
     },
     { id:"lbf", x:430, y:588, bo:3, date:"05/03 19:00", state:"未開始",
-      a:{ name:"TBD", score:null, win:false },
-      b:{ name:"TBD", score:null, win:false }
+      a:{ name:"EDGE", score:null, win:false },
+      b:{ name:"BKS", score:null, win:false }
     },
 
     // 總決賽
     { id:"gf", x:860, y:350, bo:5, date:"05/03 21:30", state:"未開始",
-      a:{ name:"TBD", score:null, win:false },
+      a:{ name:"NOOB", score:null, win:false },
       b:{ name:"TBD", score:null, win:false }
     }
   ],
